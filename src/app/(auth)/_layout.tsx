1fb2478816b1d846365ae8fe52f208a7da891e6e
Matthew Chisolm <39521893+mchisolm0@users.noreply.github.com>
2025-07-11 09:47:11 -0500

    feat: remove headers from auth screens

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -8,5 +8,11 @@ export default function GuestLayout() {
     return <Redirect href="/" />
   }
 
-  return <Stack />
+  return (
+    <Stack>
+      <Stack.Screen name="sign-in" options={{ headerShown: false }} />
+      <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+      <Stack.Screen name="reset-password" options={{ headerShown: false }} />
+    </Stack>
+  )
 }
